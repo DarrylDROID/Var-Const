7f47d2c34585e23794f9c41c4168d14dd03a28cd
Oey Darryl Valencio Wijaya <70695383+DarrylDROID@users.noreply.github.com>
2022-03-08 02:03:57 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,22 @@
+// Const
+let 日本: String = "Japan"
+print(日本)
+
+// Const
+let 🏹: String = "Marksman"
+print(🏹)
+
+// Var bool
+var isBanned = false
+print(isBanned)
+
+// Var with data type
+var winRate: Double = 45.36
+print(winRate)
+winRate = 52.76
+print(winRate)
+
+// Const
+let heroName: String = "Tigreal"
+print(heroName)
+heroName = "Rafaela"
